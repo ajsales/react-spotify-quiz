7cf864e8b267f3a3a9c8da3c88b0c6bee68472b0
Adrian Jules Sales <ajsales@berkeley.edu>
2021-04-28 00:21:30 -0700

    Added cors

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,9 @@ export default function App() {
 	const server = process.env.NODE_ENV === 'production'
 					? "https://react-spotify-quiz-back-end.herokuapp.com"
 					: "http://localhost:8081";
-	const [ socket, setSocket ] = useState(io(server));
+	const [ socket, setSocket ] = useState(io(server, {
+		withCredentials: true
+	}));
 
 	const handlePlayer = (player) => {
 		socket.emit('player', player);
